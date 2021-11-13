57cf083a7d4fcbdb7f9d4a9a1de242e3b66ecfca
Igor Nikiforov <desola2@inbox.ru>
2021-11-13 18:08:54 +0300

    Initial commit

---
+++ controllers/controller.js
@@ -0,0 +1,18 @@
+// const games_model = require('../models/games_model');
+const resp = require('../baseball.json')
+const resp2 = require('../baseball2.json')
+const resp3 = require('../baseball3.json')
+const resp4 = require('../baseball4.json')
+
+let counter = 0;
+
+const respArr = [resp, resp2, resp3, resp4]
+
+class Controller {
+  async getGame(req, res) {
+    res.status(200).send(respArr[counter]);
+		counter = counter <= 2 ? counter + 1 : 0
+  }
+}
+
+module.exports = new Controller();
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = require('./routes/Routes');
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(function (req, res, next) {
+  // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.24:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+app.use('/', router);
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('Page not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
