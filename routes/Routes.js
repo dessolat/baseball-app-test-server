04d295b438005de9aa7684085c74284e50142652
Igor Nikiforov <desola2@inbox.ru>
2021-11-14 15:31:46 +0300

    Add /:count route to Routes

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const Controller = require('../controllers/controller')
 const router = new Router();
 
+router.get('/:count', Controller.getGameStart);
 router.get('/', Controller.getGame);
 
 module.exports = router;
