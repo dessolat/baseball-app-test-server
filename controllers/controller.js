71ec507c6968c25466112a78ff34404805c00b91
Igor Nikiforov <desola2@inbox.ru>
2021-11-14 14:50:16 +0300

    Add 5-th json

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,15 +2,16 @@ const resp = require('../baseball.json')
 const resp2 = require('../baseball2.json')
 const resp3 = require('../baseball3.json')
 const resp4 = require('../baseball4.json')
+const resp5 = require('../baseball5.json')
 
 let counter = 0;
 
-const respArr = [resp, resp2, resp3, resp4]
+const respArr = [resp, resp2, resp3, resp4, resp5]
 
 class Controller {
   async getGame(req, res) {
     res.status(200).send(respArr[counter]);
-		counter = counter <= 2 ? counter + 1 : 0
+		counter = counter <= 3 ? counter + 1 : 0
   }
 }
 
